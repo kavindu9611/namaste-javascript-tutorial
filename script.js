04f5e9246ebddb4a71242ff1268297e88cc1b4ef
kavindu9611 <kavinduwickramasinghe1920@gmail.com>
2025-01-28 22:23:26 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const c = 80;
+var es = 345
+{
+  let blockLet = 44
+  var blockVar = 22
+  console.log("block")
+}
+
+function x() {
+  const c = 30;
+  var e= 76
+  console.log(c);
+}
+
+x();
+console.log(c);
