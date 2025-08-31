facd1c69c5a72c9994cc8e439f468eb4928c7b79
kavindu9611 <kavinduwickramasinghe1920@gmail.com>
2025-09-01 01:08:36 +0530

    Fix:Formatting issue

--- 19-map-filter-reduce/map.js
+++ 19-map-filter-reduce/map.js
@@ -1,21 +1,21 @@
-const arr  = [5,1,3,2,6]
+const arr = [5, 1, 3, 2, 6];
 
-function double(x){
-    return x * 2
+function double(x) {
+  return x * 2;
 }
 
-function triple(x){
-    return x * 3
+function triple(x) {
+  return x * 3;
 }
 
-function binary(x){
-    return x.toString(2)
+function binary(x) {
+  return x.toString(2);
 }
 
-const output1 = arr.map(double)
-const output2 = arr.map(triple)
-const output3 = arr.map(binary)
+const output1 = arr.map(double);
+const output2 = arr.map(triple);
+const output3 = arr.map(binary);
 
-console.log(output1)
-console.log(output2)
-console.log(output3)
+console.log(output1);
+console.log(output2);
+console.log(output3);
