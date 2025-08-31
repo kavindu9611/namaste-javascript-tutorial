097439d3b22081c47f173d05421aba84b3b6580f
kavindu9611 <kavinduwickramasinghe1920@gmail.com>
2025-09-01 00:40:27 +0530

    25-09-01

---
+++ 03-hoisting/ex1.js
@@ -0,0 +1,9 @@
+
+var x =7
+
+function getName(){
+  console.log("test getName")
+}
+
+getName();
+console.log(x)
--- script.js
+++ script.js
@@ -1,17 +1,1 @@
 
-const c = 80;
-var es = 345
-{
-  let blockLet = 44
-  var blockVar = 22
-  console.log("block")
-}
-
-function x() {
-  const c = 30;
-  var e= 76
-  console.log(c);
-}
-
-x();
-console.log(c);
