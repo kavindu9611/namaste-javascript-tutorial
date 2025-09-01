83d0f5b958e5e99ded77a56d2cf13b6d08b22a40
kavindu9611 <kavinduwickramasinghe1920@gmail.com>
2025-09-01 11:49:49 +0530

    Javascript reduce function

---
+++ 19-map-filter-reduce/reduce.js
@@ -0,0 +1,47 @@
+//Use at a place where you have to take all the elements of a array
+//and come up with a single value
+
+//sum or max
+
+const arr = [5, 1, 3, 2, 6];
+
+function findSum(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  return sum;
+}
+
+console.log(findSum(arr));
+
+const output = arr.reduce(function (acc, curr) {
+  acc = acc + curr;
+  return acc;
+}, 0);
+
+console.log(output);
+
+//Find max value from array
+
+function findMax(arr) {
+  let max = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
+console.log(findMax(arr));
+
+const maxOutput = arr.reduce(function(max,curr){
+if(curr > max){
+  max = curr
+}
+return max
+},0)
+
+console.log(maxOutput);
