f843d55da849010361f7ecf9f397d78bcc37fa64
kavindu9611 <kavinduwickramasinghe1920@gmail.com>
2025-09-03 00:40:12 +0530

    Introduction to the promises

---
+++ promises-s02-ep2/index.js
@@ -0,0 +1,25 @@
+//Pomises are used to handle async operations in javascript
+
+ //Before promises
+
+ const cart = ["shoes", "pants", "kurta"]
+
+ createOrder(cart, function(){
+    proceedToPayment(ordrId)
+ })
+
+ //createOrder return orderId and it s responsible for calling proceedToPayment
+ //this is a issue (Inverson of control)
+
+
+ //After Promise
+
+ const promise = createOrder(cart)
+
+ promise.then(function(orderId){
+    proceedToPayment(orderId)
+ })
+
+
+//Earlier we are passing a callback function to another function by blindly trusting it
+// And with promise we a re attaching a function to a promise object  
